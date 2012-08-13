2cfbf7fee1e62564bf196ff9df20c17a825e9093
Mercury <info@harisinfo.co.uk>
2012-08-13 18:43:24 +0100

    Small change

--- html/modules.config.inc.php
+++ html/modules.config.inc.php
@@ -62,7 +62,8 @@ $registered_modules['home']['template'] = 'home.tpl';
 
 $registered_modules['product']['name'] = 'product';
 $registered_modules['product']['manager_name'] = 'ProductManager';
-$registered_modules['product']['template_variable'] = 'product_response';
+$registered_modules['home']['attach_module']['CategoryManager'] = 'CategoryManager';
+$registered_modules['home']['attach_module']['LoginManager'] = 'LoginManager';
 $registered_modules['product']['template'] = 'index.tpl';
 
 // content
