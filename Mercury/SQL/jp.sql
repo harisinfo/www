9c93136fb8edacb671d3c76409aa4c61aa1582ca
Mercury <info@harisinfo.co.uk>
2012-08-16 18:03:47 +0100

    Changes to DB structure

--- Mercury/SQL/jp.sql
+++ Mercury/SQL/jp.sql
@@ -3,7 +3,7 @@
 -- Server version:               5.1.53-community-log - MySQL Community Server (GPL)
 -- Server OS:                    Win64
 -- HeidiSQL version:             7.0.0.4053
--- Date/time:                    2012-08-15 18:24:24
+-- Date/time:                    2012-08-16 18:03:19
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -47,32 +47,11 @@ REPLACE INTO `category` (`category_id`, `date_created`, `date_modified`, `langua
 /*!40000 ALTER TABLE `category` ENABLE KEYS */;
 
 
--- Dumping structure for table jp.condition_product
-DROP TABLE IF EXISTS `condition_product`;
-CREATE TABLE IF NOT EXISTS `condition_product` (
-  `condition_product_id` int(50) NOT NULL AUTO_INCREMENT,
-  `product_group_id` int(10) DEFAULT '0',
-  `dispense` int(3) DEFAULT '0',
-  `volume` int(10) DEFAULT '0',
-  `volume_units` int(10) DEFAULT '0',
-  `quantity` int(10) DEFAULT '0',
-  `strength` int(10) DEFAULT '0',
-  `strength_units` int(10) DEFAULT '0',
-  `rrp` float(10,2) DEFAULT '0.00',
-  `shipping_cost` float(10,2) DEFAULT '0.00',
-  `hide_product` tinyint(1) DEFAULT '1',
-  PRIMARY KEY (`condition_product_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
--- Dumping data for table jp.condition_product: 0 rows
-/*!40000 ALTER TABLE `condition_product` DISABLE KEYS */;
-/*!40000 ALTER TABLE `condition_product` ENABLE KEYS */;
-
-
 -- Dumping structure for table jp.medical_condition
 DROP TABLE IF EXISTS `medical_condition`;
 CREATE TABLE IF NOT EXISTS `medical_condition` (
   `medical_condition_id` int(10) NOT NULL AUTO_INCREMENT,
+  `sub_category_id` int(10) NOT NULL DEFAULT '0',
   `medical_condition_label` varchar(50) DEFAULT NULL,
   `medical_condition_label_en_UK` varchar(50) DEFAULT NULL,
   `gender` smallint(1) DEFAULT NULL COMMENT '0 - Male, 1 - Female, 2 - No Preference',
@@ -82,11 +61,25 @@ CREATE TABLE IF NOT EXISTS `medical_condition` (
 
 -- Dumping data for table jp.medical_condition: 3 rows
 /*!40000 ALTER TABLE `medical_condition` DISABLE KEYS */;
-REPLACE INTO `medical_condition` (`medical_condition_id`, `medical_condition_label`, `medical_condition_label_en_UK`, `gender`, `flag_hide`) VALUES
-	(1, 'Weight Loss', 'Weight Loss', 2, 0),
-	(2, 'Emergency Contraception', 'Emergency Contraception', 2, 0),
-	(3, 'Hair Loss', 'Hair Loss', 0, 0);
+REPLACE INTO `medical_condition` (`medical_condition_id`, `sub_category_id`, `medical_condition_label`, `medical_condition_label_en_UK`, `gender`, `flag_hide`) VALUES
+	(1, 0, 'Weight Loss', 'Weight Loss', 2, 0),
+	(2, 0, 'Emergency Contraception', 'Emergency Contraception', 2, 0),
+	(3, 2, 'Hair Loss', 'Hair Loss', 0, 0);
 /*!40000 ALTER TABLE `medical_condition` ENABLE KEYS */;
+
+
+-- Dumping structure for table jp.medical_condition_product
+DROP TABLE IF EXISTS `medical_condition_product`;
+CREATE TABLE IF NOT EXISTS `medical_condition_product` (
+  `medical_condition_product_id` int(50) NOT NULL AUTO_INCREMENT,
+  `medical_condition_id` int(10) NOT NULL DEFAULT '0',
+  `product_id` int(10) DEFAULT '0',
+  PRIMARY KEY (`medical_condition_product_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- Dumping data for table jp.medical_condition_product: 0 rows
+/*!40000 ALTER TABLE `medical_condition_product` DISABLE KEYS */;
+/*!40000 ALTER TABLE `medical_condition_product` ENABLE KEYS */;
 
 
 -- Dumping structure for table jp.medical_condition_question
@@ -103,7 +96,7 @@ CREATE TABLE IF NOT EXISTS `medical_condition_question` (
   PRIMARY KEY (`medical_condition_question_id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=26 DEFAULT CHARSET=latin1;
 
--- Dumping data for table jp.medical_condition_question: 22 rows
+-- Dumping data for table jp.medical_condition_question: 25 rows
 /*!40000 ALTER TABLE `medical_condition_question` DISABLE KEYS */;
 REPLACE INTO `medical_condition_question` (`medical_condition_question_id`, `medical_condition_question_group`, `sort_order`, `medical_condition_id`, `question_id`, `flag_required`, `validation_rule`, `show_medical_condition_question`) VALUES
 	(1, 1, 1, 1, 1, 1, '', 1),
@@ -148,8 +141,13 @@ CREATE TABLE IF NOT EXISTS `product` (
   `product_title` text NOT NULL,
   `product_description` longtext NOT NULL,
   `product_attributes` longtext NOT NULL,
-  `whole_sale_price` decimal(4,2) NOT NULL DEFAULT '0.00',
-  `rrp` decimal(4,2) NOT NULL DEFAULT '0.00',
+  `whole_sale_price` decimal(5,3) NOT NULL DEFAULT '0.000',
+  `rrp` decimal(5,3) NOT NULL DEFAULT '0.000',
+  `cost_price` decimal(5,3) NOT NULL DEFAULT '0.000',
+  `selling_price` decimal(5,3) NOT NULL DEFAULT '0.000',
+  `dispense_ml` int(10) NOT NULL DEFAULT '0',
+  `dispense_mg` int(10) NOT NULL DEFAULT '0',
+  `dispense_no` int(10) NOT NULL DEFAULT '0',
   `flag_lock` tinyint(1) NOT NULL DEFAULT '0',
   `flag_hide` tinyint(1) NOT NULL DEFAULT '1',
   `flag_delete` tinyint(1) NOT NULL DEFAULT '0',
@@ -160,12 +158,14 @@ CREATE TABLE IF NOT EXISTS `product` (
   PRIMARY KEY (`product_id`),
   KEY `category_id` (`category_id`),
   KEY `sub_category_id` (`sub_category_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
--- Dumping data for table jp.product: 1 rows
+-- Dumping data for table jp.product: 3 rows
 /*!40000 ALTER TABLE `product` DISABLE KEYS */;
-REPLACE INTO `product` (`product_id`, `category_id`, `sub_category_id`, `date_created`, `date_modified`, `brand`, `product_name`, `product_keyword`, `product_title`, `product_description`, `product_attributes`, `whole_sale_price`, `rrp`, `flag_lock`, `flag_hide`, `flag_delete`, `flag_featured_product`, `flag_featured_home`, `flag_requires_questionaire`, `flag_requires_doctor_approval`) VALUES
-	(2, 4, 22, '0000-00-00 00:00:00', '2012-08-14 12:08:30', 'Bayer', 'Levonelle One-step Morning After Pill', 'Levonelle One-step Morning After Pill', 'Levonelle One-step Morning After Pill', 'Levonelle One-step Morning After Pill emergency contraception, or known as the morning-after-pill, is a form of birth control designed to be used should other methods fail or unprotected intercourse occur. Morning-after-pill is usually used the day after unprotected sexual activity.\r\n\r\nLondon Customers: Patients wishing to purchase from London Postcodes (N, NW, W, SW, EC and WC) can now receive their treatments via on the same day, absolutely FREE of shipping charge.', '', 9.99, 10.99, 0, 0, 0, 0, 0, 1, 0);
+REPLACE INTO `product` (`product_id`, `category_id`, `sub_category_id`, `date_created`, `date_modified`, `brand`, `product_name`, `product_keyword`, `product_title`, `product_description`, `product_attributes`, `whole_sale_price`, `rrp`, `cost_price`, `selling_price`, `dispense_ml`, `dispense_mg`, `dispense_no`, `flag_lock`, `flag_hide`, `flag_delete`, `flag_featured_product`, `flag_featured_home`, `flag_requires_questionaire`, `flag_requires_doctor_approval`) VALUES
+	(3, 2, 12, '0000-00-00 00:00:00', '2012-08-16 15:25:53', 'Regaine', 'Regaine Foam Single Pack', 'Regaine Foam Single Pack', 'Regaine Foam Single Pack', 'REGAINE® Foam is the first and only clinically proven foam hair loss treatment for men. It works by increasing the blood supply to the hair follicles, which helps to strengthen existing hair and stimulate secondary hair growth. The easy-to-use foam takes very little time to apply, meaning it seamlessly fits into your daily grooming routine.\r\n\r\n\r\nHelps prevent further hair loss and regrow hair\r\nResults may be noticeable in just 8 weeks\r\nApply directly to your scalp—twice a day, every day\r\nGoes on easily and dries quickly\r\nIs unscented\r\nREGAINE® Foam is licensed for hereditary hair loss in Men aged 18 to 49. The cut off age of 49 is based on the upper age limit in the trials submitted for license.', '', 9.999, 34.950, 0.000, 0.000, 60, 0, 0, 0, 0, 0, 1, 1, 1, 0),
+	(2, 4, 22, '0000-00-00 00:00:00', '2012-08-14 12:08:30', 'Bayer', 'Levonelle One-step Morning After Pill', 'Levonelle One-step Morning After Pill', 'Levonelle One-step Morning After Pill', 'Levonelle One-step Morning After Pill emergency contraception, or known as the morning-after-pill, is a form of birth control designed to be used should other methods fail or unprotected intercourse occur. Morning-after-pill is usually used the day after unprotected sexual activity.\r\n\r\nLondon Customers: Patients wishing to purchase from London Postcodes (N, NW, W, SW, EC and WC) can now receive their treatments via on the same day, absolutely FREE of shipping charge.', '', 9.990, 10.990, 0.000, 0.000, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0),
+	(4, 2, 12, '0000-00-00 00:00:00', '2012-08-16 15:26:02', 'Regaine', 'Regaine Foam Triple Pack', 'Regaine Foam Triple Pack', 'Regaine Foam Triple Pack', 'REGAINE® Foam is the first and only clinically proven foam hair loss treatment for men. It works by increasing the blood supply to the hair follicles, which helps to strengthen existing hair and stimulate secondary hair growth. The easy-to-use foam takes very little time to apply, meaning it seamlessly fits into your daily grooming routine.\r\n\r\n\r\nHelps prevent further hair loss and regrow hair\r\nResults may be noticeable in just 8 weeks\r\nApply directly to your scalp—twice a day, every day\r\nGoes on easily and dries quickly\r\nIs unscented\r\nREGAINE® Foam is licensed for hereditary hair loss in Men aged 18 to 49. The cut off age of 49 is based on the upper age limit in the trials submitted for license.', '', 9.999, 69.900, 0.000, 0.000, 60, 0, 3, 0, 0, 0, 1, 1, 1, 0);
 /*!40000 ALTER TABLE `product` ENABLE KEYS */;
 
 
@@ -220,7 +220,7 @@ CREATE TABLE IF NOT EXISTS `question` (
   PRIMARY KEY (`question_id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=27 DEFAULT CHARSET=latin1;
 
--- Dumping data for table jp.question: 23 rows
+-- Dumping data for table jp.question: 26 rows
 /*!40000 ALTER TABLE `question` DISABLE KEYS */;
 REPLACE INTO `question` (`question_id`, `question_text`, `question_text_en_UK`, `question_template`, `default_selection`, `show_question_global`) VALUES
 	(1, 'To date, have you ever taken any weight loss prescription treatments such as Reductil, Xenical, Acomplia or Phentermine? If yes which one and when did you last take this treatment?', 'To date, have you ever taken any weight loss prescription treatments such as Reductil, Xenical, Acomplia or Phentermine? If yes which one and when did you last take this treatment?', 'YES_MORE_DETAILS_NO', 'NO', 1),
@@ -283,7 +283,7 @@ REPLACE INTO `sub_category` (`sub_category_id`, `date_created`, `date_modified`,
 	(9, '0000-00-00 00:00:00', '2012-08-13 14:10:44', 'en-GB', 2, 'Eczema &amp; Psoriasis', 'Eczema &amp; Psoriasis', 0, 0, 0),
 	(10, '0000-00-00 00:00:00', '2012-08-13 14:10:44', 'en-GB', 2, 'Excessive Sweating', 'Excessive Sweating', 0, 0, 0),
 	(11, '0000-00-00 00:00:00', '2012-08-13 14:10:44', 'en-GB', 2, 'Fungal Infections', 'Fungal Infections', 0, 0, 0),
-	(12, '0000-00-00 00:00:00', '2012-08-13 14:10:44', 'en-GB', 2, 'Hairloss', 'Hairloss', 0, 0, 0),
+	(12, '0000-00-00 00:00:00', '2012-08-16 15:16:24', 'en-GB', 2, 'Hair Loss', 'Hair Loss', 0, 0, 0),
 	(13, '0000-00-00 00:00:00', '2012-08-13 14:10:44', 'en-GB', 2, 'Headlice &amp; Scabies', 'Headlice &amp; Scabies', 0, 0, 0),
 	(14, '0000-00-00 00:00:00', '2012-08-13 17:54:59', 'en-GB', 2, 'Impotence (Erectile Dysfunction)', 'Impotence (Erectile Dysfunction)', 0, 0, 0),
 	(15, '0000-00-00 00:00:00', '2012-08-13 14:10:44', 'en-GB', 2, 'Jock Itch', 'Jock Itch', 0, 0, 0),
