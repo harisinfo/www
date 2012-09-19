e1bc18cb88b1a88d9fd1ae103cf69a82c3e4e355
Hari Ramamurthy <info@harisinfo.co.uk>
2012-09-19 22:05:28 +0100

    Test Protocol on Mac

---
+++ html/TestEntryPoint.php
@@ -0,0 +1,65 @@
+<?php
+session_start();
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+ini_set("include_path","C:\\wamp\\www\\Mercury");
+
+include_once('config.inc.php');
+
+if(isset($_REQUEST['application'])===TRUE)
+{
+	$include_site_config = mysql_escape_string($_REQUEST['application']).".config.inc.php";
+}
+
+if(!@include($include_site_config)) die_with_header(404, "$include_site_config not found, incorrect application settings.");
+
+
+include_once( __APPLICATIONS_ROOT . '/' . __APPLICATION_DIR . '/' .'constants.config.inc.php');
+include_once('dataconnect.php');
+
+// Include Plugins
+include_once( __PLUGIN_DIR . '/Smarty/libs/Smarty.class.php');
+
+// Include Core Modules
+include_once( __CORE_DIR . '/Factory/ConfigManager.class.php');
+include_once( __CORE_DIR . '/RequestManager/processRequest.php');
+include_once( __CORE_DIR . '/Factory/TemplateHandler.php');
+include_once( __CORE_DIR . '/DBManager/dbManager.class.php');
+
+// Include Custom Modules, Example with login
+include_once( __APPLICATIONS_ROOT . '/' . __APPLICATION_DIR . '/' . __MODULE_DIR .'/LoginManager/LoginManager.class.php');
+
+// Create dispatcher, Modules loaded on the fly - Factory
+$dispatch = create_dispatcher($request);
+$login_manager = new LoginManager($request);
+$login_response = $login_manager->checkIfLoggedIn($request,$dispatch);
+
+// Handle dispatch
+if(isset($dispatch['requires_login'])===TRUE && $dispatch['requires_login']===TRUE)
+{
+	if(isset($login_response['login'])===TRUE&&$login_response['login']==1
+	&&isset($login_response['user_id'])===TRUE&&is_numeric($login_response['user_id'])===TRUE)
+	{
+		// all is good, do something with this later
+	}
+	else
+	{
+		unset($dispatch);
+		$dispatch = create_dispatcher();
+	}
+}
+
+if(isset($dispatch['class_name'])===TRUE)
+{
+	$module = new $dispatch['class_name'];
+	$response = $module->init($request);
+	
+	$smarty->assign($dispatch['template_variable'],$response);
+	$smarty->assign('request',$request);
+	$smarty->display($dispatch['template']);
+}
+else
+{
+	die_with_header(500,"Internal Server Error, application configuration error");
+	exit;
+}
